f8cd6bf8d3d9154d5f7457fd943e1ba8fba5b4b4
alaanaldous <alaanaldous@gmail.com>
2020-08-27 03:07:34 +0300

    very bad

--- add-content.js
+++ add-content.js
@@ -2,6 +2,7 @@ var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 
+function test(){
 if (hourNow > 18) {
     greeting = 'Good evening!';
 }
@@ -16,3 +17,33 @@ else {
 }
 
 document.write('<h3>' + greeting + '</h3>');
+} 
+test();
+
+// just for practice
+function sayHello() {
+    document.write('Hello!');
+}
+
+sayHello();
+
+
+// just for practice
+
+var pass = 50;
+var score = 75;
+var msg;
+
+function alaaPassed() { 
+    grade = window.prompt('write Alaa\'s grade' , 'here' );
+    document.write("<h1 align=center> Good jog " + Teacher + " </h1>"); 
+    if (score >= pass) {
+        msg = 'Congratulations Alaa, you passed!';
+    } else {
+        msg = 'There is no way to fail of course you passed Alaa';
+    }
+
+    var el = document.getElementById('answer');
+    el.textContent = msg;
+}
+alaaPassed();
